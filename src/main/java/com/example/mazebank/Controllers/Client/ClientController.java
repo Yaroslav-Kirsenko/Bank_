7c8ac1106da0858694cd479c29e3a7bbfbaf1af5
Yaroslav <ludmilajaroslav128@gmail.com>
2024-02-21 19:22:44 +0200

    Dashboard

--- src/main/java/com/example/mazebank/Controllers/Client/ClientController.java
+++ src/main/java/com/example/mazebank/Controllers/Client/ClientController.java
@@ -1,4 +1,22 @@
 package com.example.mazebank.Controllers.Client;
 
-public class ClientController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientController implements Initializable {
+    private Button accounts_btn;
+    private Button dashboard_btn;
+    private Button logout_btn;
+    private Button profile_btn;
+    private Button report_btn;
+    private Button transaction_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
